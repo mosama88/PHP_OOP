2fdd2517e19af7d61d31b0c12a99c69a7217cd86
mosama88 <mosama88@hotmail.com>
2024-10-02 14:19:01 +0300

    first commit

---
+++ profile.php
@@ -0,0 +1,53 @@
+<?php require_once('inc/header.php') ?>
+
+<body class="vertical  light rtl ">
+    <div class="wrapper">
+
+        <!-- Start nav -->
+        <?php require_once('inc/nav.php') ?>
+        <!-- End nav -->
+
+        <!-- Start sidebar -->
+        <?php require_once('inc/sidebar.php') ?>
+
+
+
+        <div class="row">
+            <div class="col-6 mx-auto my-4">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+<td></td>
+<td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Start Footer -->
+        <?php require_once('inc/footer.php') ?>
+        <!-- End Footer -->
